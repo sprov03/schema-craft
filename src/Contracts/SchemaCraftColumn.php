d28afb46cbd127b3083bcb5985f21e8ce45cfdcf
Shawn <sprov03@gmail.com>
2026-06-04 15:06:08 -0500

    Now we have fixed the sdk generation, and now we can add data to the api, Lets, go ahead and test this in the main app again

--- src/Contracts/SchemaCraftColumn.php
+++ src/Contracts/SchemaCraftColumn.php
@@ -37,3 +37,4 @@ use Illuminate\Contracts\Database\Eloquent\Castable;
  * Extend one of those rather than implementing this interface directly.
  */
 interface SchemaCraftColumn extends Castable, FilamentRenderable, FormatsApiOutput, GeneratesFakerValue, GeneratesSdkType, ParsesApiInput, SchemaCraftType {}
+
